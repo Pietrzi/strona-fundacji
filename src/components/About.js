4980be7f82ba2ff507901eb9a543b6d15f14cf2b
Pietrzi <pietrzi@op.pl>
2020-12-11 11:55:27 +0100

    big part of about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import marta from '../assets/marta.jpg';
+import edyta from '../assets/edyta.jpg';
+import kasia from '../assets/kasia.jpg';
+import basia from '../assets/basia.jpg';
 
 const About = () => {
     return (
-        <section>
-            <h1>O fundacji</h1>
+        <section className="about-section">            
+            <div className="team-container">
+                <div className="member-box">
+                    <img src={marta} alt="marta" className="member-img"/>
+                    <p className="member-name">Marta Osadkowska</p>
+                    <p className="member-title">Prezes Fundacji</p>
+                </div>
+                <div className="member-box">
+                    <img src={edyta} alt="edyta" className="member-img"/>
+                    <p className="member-name">Edyta Ogrodowiak</p>
+                    <p className="member-title">Zarząd Fundacji</p>
+                </div>
+                <div className="member-box">
+                    <img src={kasia} alt="kasia" className="member-img"/>
+                    <p className="member-name">Katarzyna Dołżycka</p>
+                    <p className="member-title">Rada Fundacji</p>
+                </div>
+                <div className="member-box">
+                    <img src={basia} alt="basia" className="member-img"/>
+                    <p className="member-name">Barbara Gajos</p>
+                    <p className="member-title">Rada Fundacji</p>
+                </div>
+            </div>
         </section>
     )
 }
