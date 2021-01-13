00fe746fad7409637c753c15ec57ed8321f3ffd0
Pietrzi <pietrzi@op.pl>
2021-01-13 16:58:26 +0100

    rest of statut and phone nr

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <section>
-            <h1>Kontakt</h1>
+        <section className="contact-section">
+            <div className="phone-box">
+                <h2>Zadzwoń do nas</h2>
+                <h4>786 - 886 - 779</h4>
+            </div>
         </section>
     )
 }
