5c174307f051788bcdb7bebcb181f729b0499b39
Pietrzi <pietrzi@op.pl>
2021-02-13 16:34:09 +0100

    support part

--- src/components/Support.js
+++ src/components/Support.js
@@ -5,7 +5,8 @@ const Support = () => {
         <section className="support-section">
             <div className="support-container">
                 <h2>Wesprzyj naszą fundację</h2>
-                <h3>Nr konta: </h3>
+                <h3>Nr konta: 54 1600 1462 1862 7655 0000 0001</h3>
+                <h3>Tytułem przelewu: wspieram</h3>
             </div>
         </section>
     )
