04383debae9a374bce9afd4ea014787e45c23ee7
Pietrzi <pietrzi@op.pl>
2021-02-16 18:34:37 +0100

    new graphics for support and contact sections

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,10 +3,17 @@ import React from 'react';
 const Contact = () => {
     return (
         <section className="contact-section">
-            <div className="phone-box">
-                <h2>Zadzwoń do nas</h2>
-                <h4>786 - 886 - 779</h4>
-                <h4>kontakt@widzialnedzieci.pl</h4>
+            <div className="contact-container">
+                <div className="phone-box">
+                    <h2>Zadzwoń do nas</h2>
+                    <h4>786 - 886 - 779</h4>
+                    <h4>kontakt@widzialnedzieci.pl</h4>
+                </div>
+                <div className="contact-graphic"></div>
+            </div>
+            <div className="socials-holder">
+                <a href="https://www.facebook.com/Fundacja-Widzialne-Dzieci-107014821423632" target="_blank" rel="noreferrer"><div className="face"></div></a>
+                <a href="https://instagram.com/widzialnedzieci?igshid=7t72o1u2326z" target="_blank" rel="noreferrer"><div className="inst"></div></a>
             </div>
         </section>
     )
--- src/components/Support.js
+++ src/components/Support.js
@@ -3,10 +3,12 @@ import React from 'react'
 const Support = () => {
     return (
         <section className="support-section">
-            <div className="support-container">
-                <h2>Wesprzyj naszą fundację</h2>
-                <h3>Nr konta: 54 1600 1462 1862 7655 0000 0001</h3>
-                <h3>Tytuł przelewu: wspieram</h3>
+            <div className="support-wrapper">
+                <div className="support-graphic"></div>
+                <div className="support-container">
+                    <h2>Wesprzyj naszą fundację</h2>
+                    <h3>Nr konta: 54 1600 1462 1862 7655 0000 0001</h3>
+                </div>
             </div>
             <div className="socials-holder">
                 <a href="https://www.facebook.com/Fundacja-Widzialne-Dzieci-107014821423632" target="_blank" rel="noreferrer"><div className="face"></div></a>
