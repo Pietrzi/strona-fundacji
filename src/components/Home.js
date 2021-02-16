23eb5e7d4398742ca76c982772cf5b67956f25a2
Pietrzi <pietrzi@op.pl>
2021-02-16 17:35:05 +0100

    social icons and minor corrections for evaluation

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,10 @@ const Home = () => {
                 <h2>Dziecko chce być dobre. Jeśli nie umie – naucz. Jeśli nie wie – wytłumacz. Jeśli nie może – pomóż.</h2>
                 <h2 className="author">~ Janusz Korczak</h2>
             </div>
+            <div className="socials-holder">
+                <a href="https://www.facebook.com/Fundacja-Widzialne-Dzieci-107014821423632" target="_blank" rel="noreferrer"><div className="face"></div></a>
+                <a href="https://instagram.com/widzialnedzieci?igshid=7t72o1u2326z" target="_blank" rel="noreferrer"><div className="inst"></div></a>
+            </div>
         </header>
     )
 }
--- src/components/Support.js
+++ src/components/Support.js
@@ -6,7 +6,11 @@ const Support = () => {
             <div className="support-container">
                 <h2>Wesprzyj naszą fundację</h2>
                 <h3>Nr konta: 54 1600 1462 1862 7655 0000 0001</h3>
-                <h3>Tytułem przelewu: wspieram</h3>
+                <h3>Tytuł przelewu: wspieram</h3>
+            </div>
+            <div className="socials-holder">
+                <a href="https://www.facebook.com/Fundacja-Widzialne-Dzieci-107014821423632" target="_blank" rel="noreferrer"><div className="face"></div></a>
+                <a href="https://instagram.com/widzialnedzieci?igshid=7t72o1u2326z" target="_blank" rel="noreferrer"><div className="inst"></div></a>
             </div>
         </section>
     )
